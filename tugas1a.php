8b01a29bd16706a4f692fab2fd3fd5a071febad6
FikriRidhor808 <148334481+FikriRidhor808@users.noreply.github.com>
2025-09-27 14:14:45 +0700

    Update tugas1a.php

--- tugas1a.php
+++ tugas1a.php
@@ -6,14 +6,15 @@ $variabel = '{"nama":"Budi","umur":25,"hobi":["Olahraga","Musik","Makan"]}';
 // Decode ke dalam bentuk Object (default)
 $obj = json_decode($variabel);
 echo "Hasil Decode ke Object:<br>";
-echo "Nama: " . $obj->nama . "<br>";        // akses pakai ->
+echo "Nama: " . $obj->nama . "<br>";        
 echo "Umur: " . $obj->umur . "<br>";
 echo "Hobi pertama: " . $obj->hobi[0] . "<br><br>";
 
 // Decode ke dalam bentuk Array (assoc = true)
 $arr = json_decode($variabel, true);
 echo "Hasil Decode ke Array:<br>";
-echo "Nama: " . $arr["nama"] . "<br>";      // akses pakai key string
+echo "Nama: " . $arr["nama"] . "<br>";      
 echo "Umur: " . $arr["umur"] . "<br>";
 echo "Hobi kedua: " . $arr["hobi"][1] . "<br>";
+
 ?>
